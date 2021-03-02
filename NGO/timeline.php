8be2e27a5ba08f45deb36d1744bd6f2ce6b58a63
amritanshisaxena <49221982+amritanshisaxena@users.noreply.github.com>
2021-03-02 21:51:50 +0530

    Add files via upload

---
+++ NGO/timeline.php
@@ -0,0 +1,113 @@
+<?php include './includes/header2.php'?>
+<?php include './includes/scrolltotop.php'?>
+<?php include './includes/whatsapp.php'?>
+<style>
+    body {
+        background-image: url('./assets/images/883500121ea6e30e20ef21e40e51ac11.jpg');
+        background-size: cover;
+        list-style-type: none;
+        text-decoration: none;
+    }
+</style>
+    <section class="timeline-first" style="height: 750px;">
+        <div class="container display-2 text-center mt-lg-5 animated  zoomIn delay-1s" style="text-shadow: 0px 0px 5px gray;">HOPE NGO</div>
+        <div class="container display-3 text-center mt-lg-5 animated fadeInLeft delay-2s" style="text-shadow: 0px 0px 5px gray;">Since 2014</div>
+        <div class="container text-center mt-lg-5 font-weight-bold animated fadeInRight delay-3s" style="font-size: 30px;">
+            The 27th of February, Hope NGO, was born in the European Union, the brainchild of Marcis Liors Skadmanis.<br> Hope NGO was officially recognised by the IX Baltic Sea NGO Forum of the Council of the Baltic Sea States on the 17th of April 2014.
+        </div>
+    </section>
+    <hr>
+    <section class="timeline-first">
+        <div class="container">
+            <div class="main-timeline">
+
+                <!-- start experience section-->
+                <div class="timeline ">
+                    <div class="icon "></div>
+                    <div class="date-content wow fadeInLeft">
+                        <div class="date-outer ">
+                            <span class="date">
+                        <span class="month">Started</span>
+                            <span class="year">2014</span>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="timeline-content wow fadeInRight ">
+                        <h5 class="title">Hope NGO &amp; By trust foundation</h5>
+                        <p class="description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur ex sit amet massa scelerisque scelerisque. Aliquam erat volutpat. Aenean interdum finibus efficitur. Praesent dapibus dolor felis, eu ultrices elit molestie.
+                        </p>
+                    </div>
+                </div>
+                <!-- end experience section-->
+
+                <!-- start experience section-->
+                <div class="timeline">
+                    <div class="icon"></div>
+                    <div class="date-content wow fadeInRight delay-1s ">
+                        <div class="date-outer">
+                            <span class="date">
+                                                        <span class="month">First milestone</span>
+                            <span class="year">2015</span>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="timeline-content wow fadeInLeft delay-1s ">
+                        <h5 class="title">Rs. 1 Lakh Raised</h5>
+                        <p class="description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur ex sit amet massa scelerisque scelerisque. Aliquam erat volutpat. Aenean interdum finibus efficitur. Praesent dapibus dolor felis, eu ultrices elit molestie.
+                        </p>
+                    </div>
+                </div>
+                <!-- end experience section-->
+
+                <!-- start experience section-->
+                <div class="timeline">
+                    <div class="icon"></div>
+                    <div class="date-content wow fadeInLeft delay-2s">
+                        <div class="date-outer">
+                            <span class="date">
+                                                        <span class="month">2nd Milestone</span>
+                            <span class="year">2017</span>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="timeline-content wow fadeInRight delay-2s">
+                        <h5 class="title">100000+ Donations made</h5>
+                        <p class="description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur ex sit amet massa scelerisque scelerisque. Aliquam erat volutpat. Aenean interdum finibus efficitur. Praesent dapibus dolor felis, eu ultrices elit molestie.
+                        </p>
+                    </div>
+                </div>
+                <!-- end experience section-->
+
+                <!-- start experience section-->
+
+                <div class="timeline">
+                    <div class="icon"></div>
+                    <div class="date-content wow fadeInRight delay-3s">
+                        <div class="date-outer">
+                            <span class="date">
+                                                        <span class="month">Recently</span>
+                            <span class="year">2019</span>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="timeline-content wow fadeInLeft delay-3s">
+                        <h5 class="title">Won NGO of the year award</h5>
+                        <p class="description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur ex sit amet massa scelerisque scelerisque. Aliquam erat volutpat. Aenean interdum finibus efficitur. Praesent dapibus dolor felis, eu ultrices elit molestie.
+                        </p>
+                    </div>
+                </div>
+                <!-- end experience section-->
+
+            </div>
+        </div>
+    </section>
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
+    <script>
+        new WOW().init();
+    </script>
+    <?php include './includes/footer.php'?>
